9370aeb734b0acfeb068c4b721715a85e62bb29a
EsraaSarhan <43532692+EsraaSarhan@users.noreply.github.com>
2018-11-22 15:02:13 +0200

    Add files via upload

---
+++ src/locations.js
@@ -0,0 +1,65 @@
+export const allLocation = [{
+  location: {
+    lat: 30.028002,
+    lng: 31.212337
+  },
+  title: 'Cairo University',
+  address: '1 University Street, Giza, Egpyt'
+},
+{
+  location: {
+    lat: 29.8668662,
+    lng: 31.3152702
+  },
+  title: 'Helwan University',
+  address: 'Al Sikka Al Hadid Al Gharbeya, Al Masaken Al Iqtisadeyah, Qism Helwan, Cairo Governorate, Egpyt'
+},
+{
+  location: {
+    lat: 30.075807,
+    lng: 31.281116
+  },
+  title: 'Ain Shams University University',
+  address: 'Nour Mosque، 38 Abbasia Next، El-Mohamady, Al Waili, Cairo, Egypt'
+},
+{
+  location: {
+    lat: 30.5655764,
+    lng: 31.0130822
+  },
+  title: 'Menoufia University',
+  address: 'Gamal Abd El-Nasir, Qism Shebeen El-Kom, Shebeen El-Kom, Menofia Governorate, Egypt'
+},
+{
+  location: {
+    lat: 30.5883084,
+    lng: 31.4831937
+  },
+  title: 'Zagazig University',
+  address: 'Shaibet an Nakareyah, Markaz El-Zakazik, Ash Sharqia Governorate 44519, Egypt'
+},
+{
+  location: {
+    lat: 31.0418464,
+    lng: 31.3574868
+  },
+  title: 'Mansoura University',
+  address: 'Mansoura University, Mit Khamis WA Kafr Al Mougi, Mansoura, Ägypten'
+},
+{
+  location: {
+    lat: 30.4725671,
+    lng: 31.1873206
+  },
+  title: 'Banha University',
+  address: ' El-Shaheed Farid Nada, Qism Banha, Banha, Al Qalyubia Governorate 13511, Egypt'
+},
+{
+  location: {
+    lat: 31.2419362,
+    lng: 32.3166432
+  },
+  title: 'Port Said University',
+  address: 'University Administration، Port Fouad, Port Said Governorate 42526, Egypt'
+}
+];
